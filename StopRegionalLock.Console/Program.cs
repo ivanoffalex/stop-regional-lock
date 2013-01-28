69f090642812a98dcf88dacce5fafa14b40da7c8
Ivanoff Alex <ivanoff.alex@gmail.com>
2013-01-28 22:08:09 +0200

    Add synchronizer console

---
+++ StopRegionalLock.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using StopRegionalLock.Data.BusinessLogic.SteamContentDescriptionRecord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopRegionalLock.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CDRSynchronizer();
+        }
+
+        private static void CDRSynchronizer()
+        {
+            CDRManager.PrepareServers();
+            CDRManager.Update();
+        }
+    }
+}
